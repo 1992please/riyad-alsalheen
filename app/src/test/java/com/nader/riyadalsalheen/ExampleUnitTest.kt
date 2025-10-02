00e3eff6dbc61dbfc690032a07d8b18aa41b7523
Nader Hegazy <eng.nadernour@gmail.com>
2025-10-02 08:27:31 +0300

    Cleaned project dependencies and configurations.

--- app/src/test/java/com/nader/riyadalsalheen/ExampleUnitTest.kt
+++ app/src/test/java/com/nader/riyadalsalheen/ExampleUnitTest.kt
@@ -1,7 +1,8 @@
 package com.nader.riyadalsalheen
 
-import org.junit.Assert.assertEquals
 import org.junit.Test
+
+import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
